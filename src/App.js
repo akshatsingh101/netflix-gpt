b454c73ab11b4efa1c0b49629580a8a9b2c1fe7f
akshatsingh007 <akshatsingh.as.20@gmail.com>
2026-03-30 19:51:43 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import Body from "./components/Body";
 
 
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-      let's start netflix-gpt
-    </div>
-  );
+    <Body />
+  )
 }
 
 export default App;
